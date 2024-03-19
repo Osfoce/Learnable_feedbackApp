93ef76f927f060cd53d157f0947866d91fb1249b
osfoce <oselufortunatus2002@gmail.com>
2024-03-19 21:16:41 +0100

    task done

--- script/run.js
+++ script/run.js
@@ -1,3 +1,5 @@
+let totalCount, addCount;
+
 const main = async () => {
 
     const helloContractFactory = await hre.ethers.getContractFactory("feedBack");
@@ -7,13 +9,48 @@ const main = async () => {
     const test = await helloContract.getMessage();
     console.log(test);
    
-    await helloContract.incrementCount();
-    await helloContract.incrementCount();
-    await helloContract.incrementCount();
+    addCount =  await helloContract.incrementCount();
+    // await helloContract.incrementCount();
+    // await helloContract.incrementCount();
 
-     await helloContract.getTotalCount();
-  
+     totalCount = await helloContract.getTotalCount();
+     document.getElementById('counter').innerHTML = totalCount
+
 }
+
+
+const submitted = () => addCount;
+const clicked = () => console.log("clicked")
+   
+const feedbackForm = document.getElementById('feedback-form');
+const feedbackList = document.getElementById('feedback-list');
+   
+async function connect() {
+    if (typeof window.ethereum !== 'undefined') {
+        ethereum.request({ method: 'eth_requestAccounts' });
+    }
+     else {  alert('Download Metamask')};
+}
+   
+   
+   
+feedbackForm.addEventListener('submit', function(event) {
+event.preventDefault();
+
+
+    const userInput = document.getElementById('user-input').value.trim();
+   
+    if (userInput !== '') {
+        const feedbackItem = document.createElement('div');
+        feedbackItem.classList.add('feedback-item');
+        feedbackItem.textContent = userInput;
+        feedbackList.appendChild(feedbackItem);
+   
+        // Clear input after submission
+        document.getElementById('user-input').value = '';
+        //document.getElementById('counter').innerHTML = totalCount;
+           }
+       });
    
    const runMain = async () => {
     try {
@@ -25,38 +62,4 @@ const main = async () => {
    }
 
    }
-   
-   runMain();
-   
-   
-   
-const submitted = () => console.log("submitted")
-const clicked = () => console.log("clicked")
-   
-// const feedbackForm = document.getElementById('feedback-form');
-// const feedbackList = document.getElementById('feedback-list');
-   
-// async function connect() {
-//     if (typeof window.we3 !== 'undefined') {
-//         ethereum.request({ method: 'eth_requestAccounts' });
-//     }
-//        alert('Download Metamask');
-// }
-   
-   
-   
-//        feedbackForm.addEventListener('submit', function(event) {
-//            event.preventDefault();
-   
-//            const userInput = document.getElementById('user-input').value.trim();
-   
-//            if (userInput !== '') {
-//                const feedbackItem = document.createElement('div');
-//                feedbackItem.classList.add('feedback-item');
-//                feedbackItem.textContent = userInput;
-//                feedbackList.appendChild(feedbackItem);
-   
-//                // Clear input after submission
-//                document.getElementById('user-input').value = '';
-//            }
-//        });
+    runMain();
