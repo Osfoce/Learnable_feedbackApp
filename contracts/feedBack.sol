941132c767c5b875136ba6c3a8c5f3ac627b1229
osfoce <oselufortunatus2002@gmail.com>
2024-03-19 10:41:55 +0100

    adjustment on script

--- contracts/feedBack.sol
+++ contracts/feedBack.sol
@@ -4,5 +4,9 @@ pragma solidity ^0.8.24;
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
-contract Lock {
+contract getFeedback (_ndex) public view returns (address, string memory, uint256) {
+Message memory item = messages[_ndex];
+
+return(item.author, itemcontent, item.timestamp)
+}
  
